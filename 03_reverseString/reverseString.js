34688dea312f56760b1c737ab7a38333713dd53b
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-07-29 15:30:19 -0400

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    reverse = "";
+    for (i = word.length; i >= 0; i--)
+        reverse += word.charAt(i);
+    
+    return reverse;
 };
 
 // Do not edit below this line
