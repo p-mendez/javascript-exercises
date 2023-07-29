c31e9d728ac66a2978f819da9de2e07f29b229af
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-07-29 16:28:28 -0400

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    for (number of arguments)
+        if (number < 0)
+            return "ERROR";
+        if (typeof number != "number")
+            return "ERROR";
+    let sum = 0;
 
+    // put in ascending order, if necessary
+    if (isDescending(num1, num2)){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+function isDescending(num1, num2) {
+    return num1 > num2;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
