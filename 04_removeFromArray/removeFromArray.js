06d0a8dc1d46e426bf47d7c0e981b7fe7bf3610c
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-07-29 16:13:52 -0400

    Complete the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (let i = 0; i < array.length; i++)  
+        for (let element of elements)
+            if (array[i] === element) {
+                array.splice(i, 1);
+                i--;
+            }
+    return array;
 };
 
 // Do not edit below this line
