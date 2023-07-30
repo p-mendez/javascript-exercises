c28b302d22801e3a1603d7170414fbc4889e3cf3
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-07-29 22:39:24 -0400

    Complete assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return nearestTenth((temp - 32) * (5/9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return nearestTenth(temp * (9/5) + 32);
 };
+
+function nearestTenth(number) {
+  return Math.round(number * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
