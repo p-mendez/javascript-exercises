ecde4ee3be451806c52637f15d8041f39e892b5d
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-04 18:39:38 -0500

    Solve the problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    wordsOnly = "";
+    for (const char of word) {
+        if (char.match(/[a-zA-Z0-9]/))
+            wordsOnly += char.toLowerCase();
+    }
 
+    if (wordsOnly.length < 2) 
+        return true;
+    
+    let firstLetter = wordsOnly.charAt(0);
+    let lastLetter = wordsOnly.slice(-1);
+
+    if (firstLetter == lastLetter)
+        return true && palindromes(wordsOnly.substring(1, wordsOnly.length - 1));
+    else
+        return false;
 };
 
 // Do not edit below this line
