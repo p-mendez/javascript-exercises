f5e7234ab363077b3be47bb4cd7e78652a402230
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-04 15:49:32 -0500

    Complete calculator assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...arr) {
+  // total = 0;
+  // for (const num of nums)
+  //   total += num;
+
+  // return total;
+  return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number <= 1)
+    return 1;
+  else
+    return number * factorial(number -1);
 };
 
 // Do not edit below this line
