8f757b42db98cc3de935d63785092121cb200119
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-04 23:09:54 -0500

    Complete assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arr) {
+    let sortedArray =  arr.sort((a, b) => {
+        let currentYear = new Date().getFullYear();
+        let aAge = (a.yearOfDeath ?? currentYear) - a.yearOfBirth;
+        let bAge = (b.yearOfDeath ?? currentYear) - b.yearOfBirth;
+        return aAge - bAge;
+    });
+    return sortedArray[sortedArray.length - 1];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
